915b4ad6fb6698592173639912a637c8a7690670
Roger Chao <chaoshuangjui@gmail.com>
2013-09-13 19:24:20 +0800

    My PHP Form1

---
+++ Welcome.php
@@ -0,0 +1,8 @@
+<html>
+	<head><title>Welcome to My Home!</title></head>
+
+	<body>
+		Welcome <?php echo $_POST["name"]; ?><br>
+		Your email address is: <?php echo $_POST["email"]; ?>
+	</body>
+</html>
---
+++ main.php
@@ -0,0 +1,11 @@
+<html>
+	<head><title>PHP Form-Post Demo</title></head>
+
+	<body>
+		<form action="Welcome.php" method="post">
+			Name: <input type="text" name="name"><br />
+			E-mail: <input type="text" name="email"><br>
+			<input type="submit">
+		</form>
+	</body>
+</html>
