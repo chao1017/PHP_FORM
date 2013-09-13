ae17a0161c5b6fdc7083b5e9f4e9cb695c559633
Roger Chao <chaoshuangjui@gmail.com>
2013-09-13 19:43:47 +0800

    add fields

--- main.php
+++ main.php
@@ -3,9 +3,15 @@
 
 	<body>
 		<form action="Welcome.php" method="post">
-			Name: <input type="text" name="name"><br />
-			E-mail: <input type="text" name="email"><br>
-			<input type="submit">
+			<label>Name:</label><input type="text" name="userName"><br><br>
+			<label>E-Mail:</label><input type="text" name="email"><br><br>
+			<label>Website:</label><input type="text" name="website"><br><br>
+			<label>Comment:</label><input type="text" name="comment"><br><br>
+			<label>Gender:</label>
+			<input type="radio" name="gender" value="female">Female
+			<input type="radio" name="gender" value="male">Male
+
 		</form>
+
 	</body>
 </html>
