c7453dcac4cb3413e58584cf3f6bf72d9a3caf85
Roger Chao <chaoshuangjui@gmail.com>
2013-10-12 16:33:52 +0800

    new

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 	<head><title>PHP Form-Post Demo</title></head>
 
 	<body>
-		<form action="Welcome.php" method="post">
+		<form action="<?php $_SERVER["PHP_SELF"];?>" method="post">
 			<label>Name:</label><input type="text" name="userName"><br><br>
 			<label>E-Mail:</label><input type="text" name="email"><br><br>
 			<label>Website:</label><input type="text" name="website"><br><br>
@@ -11,7 +11,34 @@
 			<input type="radio" name="gender" value="female">Female
 			<input type="radio" name="gender" value="male">Male
 
+			<br>
+			<input type="submit" value="Submit">
+
 		</form>
 
+		<?php
+			
+			//define variables and set to empty values
+			$nameErr = $emailErr = $genderErr = $websiteErr = "";
+			$name = $email = $gender = $comment = $website = "";
+
+			if($_SERVER["REQUEST_METHOD"] == "POST")
+			{
+				$name = test_input($_POST["name"]);
+				$email = test_input($_POST["email"]);
+				$website = test_input($_POST["website"]);
+				$comment = test_input($_POST["comment"]);
+				$gender = $_POST["gender"];
+			}
+
+			function test_input($data)
+			{
+				$data = trim($data);
+				$data = stripslashes($data);
+				$data = htmlspecialchars($data);
+
+				return $data;				
+			}
+		?>
 	</body>
 </html>
